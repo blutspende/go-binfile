8bcd3e8b07b91e7ed5084d0e215023fa1edd5eba
Stephan <node1@posteo.de>
2022-09-04 20:32:52 +0200

    unmarshal fixed

--- unmarshal.go
+++ unmarshal.go
@@ -2,414 +2,263 @@ package binfile
 
 import (
 	"fmt"
+	"log"
 	"reflect"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-type LineBreak int
-
-const (
-	NO_LINEBREAKS LineBreak = -1
-	CR            LineBreak = 0x0D
-	LF            LineBreak = 0x0A
-	CRLF          LineBreak = 0x0D0A
-)
-
-func (l LineBreak) ToString() string {
-	return string(byte(l))
-}
-
 const (
 	ANNOTATION_TRIM       = "trim"
 	ANNOTATION_TERMINATOR = "terminator"
-	ANNOTATION_ARRAY      = "array"
 )
 
-var errAbortArrayTerminator = fmt.Errorf("Aborting due to array-terminator found")
+var ErrAbortArrayTerminator = fmt.Errorf("aborting due to array-terminator found")
+var ErrInvalidAddressAnnotation = fmt.Errorf("invalid address annotation")
+var ErrAnnotatedFieldNotWritable = fmt.Errorf("annotated Field is not writable")
 
-func Unmarshal(inputStringLines []byte, v interface{}, linebreak LineBreak, arrayTerminator string) error {
+func Unmarshal(inputBytes []byte, target interface{}, arrayTerminator string) error {
 
-	/*if linebreak != NO_LINEBREAKS {
-		inputString := string(d)
-		inputStringLines := strings.Split(inputString, linebreak.ToString())
-	}*/
-
-	_, _, err := reflectInputToStruct(inputStringLines, 1, 0 /*currentbyte*/, v, arrayTerminator)
-
-	if err != nil {
-		return err
+	if reflect.TypeOf(target).Kind() != reflect.Pointer && reflect.ValueOf(target).Elem().Type().Kind() != reflect.Struct {
+		return fmt.Errorf("Unmarshal: Target '%s' is of Type %s, but require reflect.Struct", reflect.TypeOf(target).Name(), reflect.TypeOf(target))
 	}
 
-	return nil
+	targetStruct := reflect.ValueOf(target).Elem()
+
+	_, err := internalUnmarshal(inputBytes, 0, targetStruct, "\r", 1)
+
+	return err
 }
 
-func marshal(v interface{}) []byte {
-
-	return []byte{}
-}
-
-type RETV int
-
-const (
-	OK         RETV = 1
-	UNEXPECTED RETV = 2 // an exit that wont abort processing. used for skipping optional records
-	ERROR      RETV = 3 // a definite error that stops the process
-)
-
-func reflectInputToStruct(bufferedBytes []byte, depth int, currentByte int, targetStruct interface{}, arrayTerminator string) (int, RETV, error) {
-	if depth > 32 {
-		return currentByte, ERROR, fmt.Errorf("maximum recursion depth reached (%d). Too many nested structures ? - aborting", depth)
-	}
-
-	/*
-		if bufferedInputLines[currentInputLine] == "" {
-			// Caution : +1 might skip one; .. without could stick in loop
-			return currentInputLine + 1, UNEXPECTED, errors.New(fmt.Sprintf("Empty Input"))
-		}
-	*/
-
-	var targetStructType reflect.Type
-	var targetStructValue reflect.Value
-	if reflect.TypeOf(targetStruct).Kind() == reflect.Struct {
-		targetStructType = reflect.TypeOf(targetStruct)
-		targetStructValue = reflect.ValueOf(targetStruct)
-	} else {
-		targetStructType = reflect.TypeOf(targetStruct).Elem()
-		targetStructValue = reflect.ValueOf(targetStruct).Elem()
-	}
-
-	for i := 0; i < targetStructType.NumField(); i++ {
-		currentRecord := targetStructValue.Field(i)
-
-		fmt.Printf("Processing Record %s\n", targetStructValue.Type().Field(i).Name)
-
-		// ftype := targetStructType.Field(i)
-		/*
-			if targetStructType.Field(i).Type.Kind() == reflect.Slice {
-				// Array of Structs
-				if reflect.TypeOf(targetStructValue.Interface()).Kind() == reflect.Struct {
-					innerStructureType := targetStructType.Field(i).Type.Elem()
-					sliceForNestedStructure := reflect.MakeSlice(targetStructType.Field(i).Type, 0, 0)
-					for currentInputLine < len(bufferedInputLines) {
-						allocatedElement := reflect.New(innerStructureType)
-						var err error
-						var retv RETV
-						currentInputLine, retv, err = reflectInputToStruct(bufferedInputLines, depth+1,
-							currentInputLine, allocatedElement.Interface())
-						if err != nil {
-							if retv == UNEXPECTED {
-								break
-							}
-							if retv == ERROR { // a serious error ends the processing
-								return currentInputLine, ERROR, err
-							}
-							sliceForNestedStructure = reflect.Append(sliceForNestedStructure, allocatedElement.Elem())
-							reflect.ValueOf(targetStruct).Elem().Field(i).Set(sliceForNestedStructure)
-						}
-						continue
-					}
-				} else if targetStructType.Field(i).Type.Kind() == reflect.Struct { // struct without annotation - descending
-					var err error
-					var retv RETV
-
-					dood := currentRecord.Addr().Interface()
-					currentInputLine, retv, err = reflectInputToStruct(bufferedInputLines, depth+1, currentInputLine, dood)
-					if err != nil {
-						if retv == UNEXPECTED {
-							if depth > 0 {
-								// if nested structures abort due to unexpected records that does not create an error
-								// as the parse will be continued one level higher
-								return currentInputLine, UNEXPECTED, err
-							} else {
-								return currentInputLine, ERROR, err
-							}
-						}
-						if retv == ERROR { // a serious error ends the processing
-							return currentInputLine, ERROR, err
-						}
-					}
-					continue
-				} else {
-					return currentInputLine, ERROR, errors.New(fmt.Sprintf("Invalid Datatype '%s' - abort unmarshal.", targetStructType.Field(i).Type.Kind()))
-				}
-			}
-		*/
-
-		if currentRecord.Kind() == reflect.Slice {
-
-			innerStructureType := targetStructType.Field(i).Type.Elem()
-			sliceForNestedStructure := reflect.MakeSlice(targetStructType.Field(i).Type, 0, 0)
-			for { // iterate for as long as the same type repeats
-				allocatedElement := reflect.New(innerStructureType)
-
-				if currentByte, err := reflectAnnotatedFields(bufferedBytes, allocatedElement.Elem(), 0, arrayTerminator); err != nil {
-					return currentByte, ERROR, fmt.Errorf("failed to process input line '%d' err:%w ", bufferedBytes[currentByte], err)
-				}
-
-				sliceForNestedStructure = reflect.Append(sliceForNestedStructure, allocatedElement.Elem())
-				reflect.ValueOf(targetStruct).Elem().Field(i).Set(sliceForNestedStructure)
-
-				// keep reading while same elements are up
-				currentByte = currentByte + 1
-				if currentByte >= len(bufferedBytes) {
-					break
-				}
-			}
-
-		} else { // The "normal" case: scanning a string into a structure :
-
-			var err error
-			currentByte, err = reflectAnnotatedFields(bufferedBytes, targetStructValue, currentByte, arrayTerminator)
-			if err != nil {
-				return currentByte, ERROR, fmt.Errorf("failed to process input line '%d' err:%w", bufferedBytes[currentByte], err)
-			}
-
-			break
-		}
-
-	}
-	return currentByte, OK, nil
-}
-
-func readFieldAddressAnnotation(annotation string) (absolute int, relativeLength int, err error) {
-	absolute = -1
-	relativeLength = -1
-	err = nil
-
-	if annotation == "" {
-		err = fmt.Errorf("no annotation")
-		return
-	}
-
-	annotationList := strings.Split(annotation, ",")
-	if annotationList[0] != "" {
-		absolute, err = strconv.Atoi(annotationList[0])
-		if err != nil {
-			return
-		}
-	}
-
-	if annotationList[1] != "" {
-		relativeLength, err = strconv.Atoi(annotationList[1])
-		if err != nil {
-			return
-		}
-	}
-
-	return
-}
-
-type DummyRec struct {
-	a string `bin:",2"`
-}
-
-// reflectAnnotatedFields - Read the whole struct
-//
-// Returns:
-//   - currentByte Position of the read-index
-//   - error
-func reflectAnnotatedFields(inputBytes []byte, record reflect.Value, currentByte int, arrayTerminator string) (int, error) {
-
-	if reflect.ValueOf(record).Type().Kind() != reflect.Struct {
-		return currentByte, fmt.Errorf("invalid type of target: '%d', expecting 'struct'", reflect.ValueOf(record).Type().Kind())
-	}
+// use this for recursion
+func internalUnmarshal(inputBytes []byte, currentByte int, record reflect.Value, arrayTerminator string, depth int) (int, error) {
 
 	for fieldNo := 0; fieldNo < record.NumField(); fieldNo++ {
 
 		recordField := record.Field(fieldNo)
-		if !recordField.CanInterface() {
-			return currentByte, fmt.Errorf("field %s is not exported - aborting import", recordField.Type().Name())
-		}
-		recordFieldInterface := recordField.Addr().Interface()
 
-		hasTrimAnnotation := false
-		hasTerminatorAnnotation := false
-		isArrayAnnotation := false
 		binTag := record.Type().Field(fieldNo).Tag.Get("bin")
-		if binTag == "" {
-			// Nested structs
-			if reflect.TypeOf(recordField.Interface()).Kind() == reflect.Struct {
-				var err error
-				currentByte, _, err = reflectInputToStruct(inputBytes, 1, currentByte, recordFieldInterface, arrayTerminator)
-				if err != nil {
-					return currentByte, err
-				}
-			}
-
-			// Not annotated and not a struct or slice -> return current position
-			continue
+		if binTag != "" && !recordField.CanInterface() {
+			return currentByte, fmt.Errorf("field '%s' is not exported but annotated", record.Type().Field(fieldNo).Name)
 		}
+
 		binTagsList := strings.Split(binTag, ",")
 		for i := 0; i < len(binTagsList); i++ {
 			binTagsList[i] = strings.Trim(binTagsList[i], " ")
 		}
+		absoluteAnnotatedPos := -1
+		relativeAnnotatedLength := -1
+		if len(binTagsList) >= 1 {
+			if !isValidAddressAnnotation(binTagsList[0]) {
+				return currentByte, ErrInvalidAddressAnnotation
+			}
+			absoluteAnnotatedPos, relativeAnnotatedLength = readAddressAnnotation(binTagsList[0])
+		}
+		hasTrimAnnotation := false
 		if sliceContainsString(binTagsList, ANNOTATION_TRIM) {
-			// the delimiter is instantly replaced with the delimiters from the file for further parsing. By default that is "\^&"
 			hasTrimAnnotation = true
 		}
+		hasTerminatorAnnotation := false
 		if sliceContainsString(binTagsList, ANNOTATION_TERMINATOR) {
 			hasTerminatorAnnotation = true
-		}
-		if sliceContainsString(binTagsList, ANNOTATION_ARRAY) {
-			isArrayAnnotation = true
-		}
-
-		absoluteAnnotatedPos, relativeAnnotatedLength, err := readFieldAddressAnnotation(binTag)
-		if err != nil {
-			return currentByte, fmt.Errorf("invalid annotation for field %s. (%w)", record.Type().Field(fieldNo).Name, err)
+			if reflect.TypeOf(recordField.Interface()).Kind() != reflect.String {
+				return currentByte, fmt.Errorf("array-terminator fields must be string ('%s')", record.Type().Field(fieldNo).Name)
+			}
 		}
 
-		fmt.Printf("Processing field %3d - %s (%d, %d)\n", currentByte, record.Type().Field(fieldNo).Name, absoluteAnnotatedPos, relativeAnnotatedLength)
-
-		/*
-			if absoluteAnnotatedPos == -1 && relativeAnnotatedLength == -1 {
-				return currentByte, errors.New(fmt.Sprintf("invalid annotation for field %s. Can not find any position", record.Type().Field(i).Name))
-			}*/
-
-		if currentByte >= len(inputBytes) || currentByte < 0 {
-			//TODO: user should be able to toggle wether he wants an exact match = error or bestfit = skip silent
-			return currentByte, nil // mapped field is beyond the data
+		if absoluteAnnotatedPos > 0 {
+			// The current field has an absolute Address. This causes the cursor to be forwarded
+			currentByte = absoluteAnnotatedPos
 		}
 
+		if relativeAnnotatedLength > 0 {
+			// Having a length, the total length is not supposed to exceed the boundaries of the input
+			if currentByte+relativeAnnotatedLength > len(inputBytes) {
+				return currentByte, fmt.Errorf("reading out of bounds position %d in input data of %d bytes", currentByte+relativeAnnotatedLength, len(inputBytes))
+			}
+		}
+
+		/* Really useful debugging:
+		for k := 0; k < depth; k++ {
+			fmt.Print(" ")
+		}
+		fmt.Printf("Field %s (%d:%d) with at %d \n",
+			record.Type().Field(fieldNo).Name,
+			absoluteAnnotatedPos, relativeAnnotatedLength, currentByte)
+		*/
 		switch reflect.TypeOf(recordField.Interface()).Kind() {
 		case reflect.Array, reflect.Slice:
 
-			if isArrayAnnotation { // Only annotated arrays are teaken into account
+			switch reflect.TypeOf(recordField.Type()).Elem().Kind() { // Nested: all here is an array of something
+			case reflect.Struct:
 
-				if reflect.TypeOf(recordField.Type()).Elem().Kind() == reflect.Struct {
+				targetType := recordField.Type()
+				output := reflect.MakeSlice(targetType, 0, 0)
+				recordField.Set(output)
 
-					targetType := recordField.Type()
-					output := reflect.MakeSlice(targetType, 0, 0)
+				for {
+					outputTarget := reflect.New(targetType.Elem())
+					lastByte := currentByte
+					var err error
+					currentByte, err = internalUnmarshal(inputBytes, currentByte, outputTarget.Elem(), arrayTerminator, depth+1)
+
+					if lastByte == currentByte { // we didnt progess a single byte
+						break
+					}
+
+					output = reflect.Append(output, outputTarget.Elem())
 					recordField.Set(output)
 
-					for {
-						outputTarget := reflect.New(targetType.Elem())
-						lastByte := currentByte
-						currentByte, _, err = reflectInputToStruct(inputBytes, 1, currentByte, outputTarget.Interface(), arrayTerminator)
-
-						if lastByte == currentByte { // we didnt progess a single byte
-							fmt.Println("Stop iterating due to no content")
-							break
-						}
-
-						output = reflect.Append(output, outputTarget.Elem())
-						recordField.Set(output)
-
-						if err == errAbortArrayTerminator {
-							fmt.Println("Leave due to terminator")
-							break // terminate because the annotated terminator-field was set
-						}
-
-						if currentByte >= len(inputBytes) { // read to an end = peaceful exit
-							fmt.Println("Leave due to end")
-							break // read further than the end
-						}
+					if err == ErrAbortArrayTerminator {
+						break // terminate because the annotated terminator-field was set
 					}
-				} else {
-					return currentByte, fmt.Errorf("Not implemented yet : Arrays of string,int,float")
+
+					if currentByte >= len(inputBytes) { // read to an end = peaceful exit
+						break // read further than the end
+					}
 				}
+
+			default:
+				return currentByte, fmt.Errorf("arrays of type '%s' are not supported (field '%s')", record.Type().Name(), record.Type().Field(fieldNo).Name)
 			}
 
-			// arrays which are not annotated are simply not processed
+		case reflect.Struct:
+			var err error
+			currentByte, err = internalUnmarshal(inputBytes, currentByte, recordField, arrayTerminator, depth+1)
+			if err != nil { // If the nested structure did fail, then bail out
+				return currentByte, err
+			}
 
 		case reflect.String:
-			if absoluteAnnotatedPos > 0 {
-				currentByte = absoluteAnnotatedPos
+
+			if relativeAnnotatedLength < 0 { // Requires a valid length
+				return currentByte, ErrInvalidAddressAnnotation
 			}
 
-			if currentByte+relativeAnnotatedLength > len(inputBytes) {
-				return currentByte, fmt.Errorf("reading out of bounds position %d in input data of %d bytes.", currentByte+relativeAnnotatedLength, len(inputBytes))
+			if !recordField.CanSet() {
+				return currentByte, ErrAnnotatedFieldNotWritable
 			}
 
 			strvalue := string(inputBytes[currentByte : currentByte+relativeAnnotatedLength])
 
 			if hasTerminatorAnnotation {
-				fmt.Println("Found the terminator ánnotation ", strvalue)
 				if strvalue == arrayTerminator {
-					return currentByte, errAbortArrayTerminator
+					// Forward by annotated length only when terminator applies
+					currentByte += relativeAnnotatedLength
+					return currentByte, ErrAbortArrayTerminator
 				}
 				break
 			}
 
 			currentByte += relativeAnnotatedLength
-
 			if hasTrimAnnotation {
 				strvalue = strings.TrimSpace(strvalue)
 			}
 
-			reflect.ValueOf(recordFieldInterface).Elem().SetString(reflect.ValueOf(strvalue).String())
+			reflect.ValueOf(recordField.Addr().Interface()).Elem().SetString(reflect.ValueOf(strvalue).String())
 
 		case reflect.Int:
-			if absoluteAnnotatedPos > 0 {
-				currentByte = absoluteAnnotatedPos
-			}
 
-			if currentByte+relativeAnnotatedLength > len(inputBytes) {
-				return currentByte, fmt.Errorf("reading out of bounds position %d in input data of %d bytes.", currentByte+relativeAnnotatedLength, len(inputBytes))
+			if relativeAnnotatedLength < 0 { // Requires a valid length
+				return currentByte, ErrInvalidAddressAnnotation
 			}
 
 			strvalue := string(inputBytes[currentByte : currentByte+relativeAnnotatedLength])
 			currentByte += relativeAnnotatedLength
 
-			if hasTrimAnnotation {
-				strvalue = strings.TrimSpace(strvalue)
-			}
+			strvalue = strings.TrimSpace(strvalue)
 
 			num, err := strconv.Atoi(strvalue)
 			if err != nil {
 				return currentByte, err
 			}
 
-			reflect.ValueOf(recordFieldInterface).Elem().Set(reflect.ValueOf(num))
+			reflect.ValueOf(recordField.Addr().Interface()).Elem().Set(reflect.ValueOf(num))
 
 		case reflect.Float32:
-			/*if absolutePos > 0 {
-				currentPosition = absolutePos
+
+			if relativeAnnotatedLength < 0 { // Requires a valid length
+				return currentByte, ErrInvalidAddressAnnotation
 			}
 
-			value := inputStr[currentPosition : currentPosition+relativeLength]
-			currentPosition += relativeLength
-
-			if hasTrimAnnotation {
-				value = strings.TrimSpace(value)
-			}
-
-			num, err := strconv.ParseFloat(value, 32)
+			value := string(inputBytes[currentByte : currentByte+relativeAnnotatedLength])
+			currentByte += relativeAnnotatedLength
+			strvalue := strings.TrimSpace(value)
+			num, err := strconv.ParseFloat(strvalue, 32)
 			if err != nil {
-				return err
+				return currentByte, err
 			}
-			reflect.ValueOf(recordFieldInterface).Elem().Set(reflect.ValueOf(float32(num)))
-			*/
+
+			reflect.ValueOf(recordField.Addr().Interface()).Elem().Set(reflect.ValueOf(float32(num)))
+
 		case reflect.Float64:
-			/*if absolutePos > 0 {
-				currentPosition = absolutePos
+
+			if relativeAnnotatedLength < 0 { // Requires a valid length
+				return currentByte, ErrInvalidAddressAnnotation
 			}
 
-			value := inputStr[currentPosition : currentPosition+relativeLength]
-			currentPosition += relativeLength
-
-			if hasTrimAnnotation {
-				value = strings.TrimSpace(value)
-			}
-
-			num, err := strconv.ParseFloat(value, 64)
+			value := string(inputBytes[currentByte : currentByte+relativeAnnotatedLength])
+			currentByte += relativeAnnotatedLength
+			strvalue := strings.TrimSpace(value)
+			num, err := strconv.ParseFloat(strvalue, 64)
 			if err != nil {
-				return err
+				return currentByte, err
 			}
-			reflect.ValueOf(recordFieldInterface).Elem().Set(reflect.ValueOf(num))
-			*/
-		case reflect.Struct:
-			// here: struct would be anotated (as opposed to the above call)
-			// currently: this shouldnt be the case
+
+			reflect.ValueOf(recordField.Addr().Interface()).Elem().Set(reflect.ValueOf(float32(num)))
+
 		default:
-			return currentByte, fmt.Errorf("invalid type of Field '%s' while trying to unmarshal", reflect.TypeOf(recordField.Interface()).Kind())
+
+			if binTag != "" { // only if annotated this wil create an error
+				return currentByte, fmt.Errorf("invalid type for field %s", record.Type().Field(fieldNo).Name)
+			}
 		}
 	}
 
 	return currentByte, nil
 }
 
+// isValidAddressAnnotation - verify the valdity of an address annotation
+// returns
+//  - true when the string matches [0-9]*:[0-9]+
+//  - false otherwise
+func isValidAddressAnnotation(str string) bool {
+	expr, err := regexp.Compile("[0-9]*:[0-9]+")
+	if err != nil {
+		log.Panic("Invalid Expression in regexp (this error should never occur)")
+		return false
+	}
+	matched := expr.Match([]byte(str))
+	return matched
+}
+
+// read an address annotation with format "absolute:length" or ":length"
+func readAddressAnnotation(str string) (int, int) {
+	abs := -1
+	length := -1
+
+	parts := strings.Split(str, ":")
+
+	if len(parts) >= 1 { // the 1st part is the absolute address
+		if parts[0] != "" {
+			abs, _ = strconv.Atoi(parts[0])
+		}
+	}
+
+	if len(parts) >= 2 { // the 2nd part is the length of the field
+		if parts[1] != "" {
+			length, _ = strconv.Atoi(parts[1])
+		}
+	}
+
+	return abs, length
+}
+
+// find a searchstring within an array of strings. only matches full
+// returns
+//   - true if the string is present
+//   - false otherwise
 func sliceContainsString(list []string, search string) bool {
 	for _, x := range list {
 		if x == search {
--- unmarshal_test.go
+++ unmarshal_test.go
@@ -7,31 +7,30 @@ import (
 )
 
 type TestBinaryStructure1 struct {
-	RecordType          string `bin:",2"`      // 0
-	UnitNo              int    `bin:",2"`      // 2
-	RackNumber          int    `bin:",4"`      // 4
-	CupPosition         int    `bin:",2"`      // 8
-	SampleType          string `bin:",1,trim"` // 10
-	SampleNo            string `bin:",4"`      // 11
-	SampleId            string `bin:",11"`     // 15
-	Dummy               string `bin:",4,trim"` // 26
-	BlockIdentification string `bin:",1,trim"` // 30
+	RecordType          string `bin:":2"`      // 0
+	UnitNo              int    `bin:":2"`      // 2
+	RackNumber          int    `bin:":4"`      // 4
+	CupPosition         int    `bin:":2"`      // 8
+	SampleType          string `bin:":1,trim"` // 10
+	SampleNo            string `bin:":4"`      // 11
+	SampleId            string `bin:":11"`     // 15
+	Dummy               string `bin:":4,trim"` // 26
+	BlockIdentification string `bin:":1,trim"` // 30
 
 	TestResults []struct {
-		TestCode   string `bin:",2"`            // 1. 30  2. 43
-		TestResult string `bin:",9,trim"`       // 1. 32  2. 45
-		Flags      string `bin:",2,trim"`       // 1. 41  2. 54
-		Terminator string `bin:",1,terminator"` // 1.     2. 56
-	} `bin:",,array"`
-
-	DoNotSkipThisOneTest string `bin:",1"` // 44 (with 1 run)
+		TestCode   string `bin:":2"`            // 1. 30  2. 43
+		TestResult string `bin:":9,trim"`       // 1. 32  2. 45
+		Flags      string `bin:":2,trim"`       // 1. 41  2. 54
+		Terminator string `bin:":1,terminator"` // 1.     2. 56
+	}
 }
 
 // POC Unmarshal binary protocol generic simple example
-/*func TestUnmarshalBinary1(t *testing.T) {
+func TestUnmarshalBinary1(t *testing.T) {
 	data := "D 03116506 044760722905768    E61     6.40  62      935  "
 	var r TestBinaryStructure1
-	err := Unmarshal([]byte(data), &r, CR, "\n")
+	err := Unmarshal([]byte(data), &r, "\r")
+	// err := Unmarshal([]byte(data), &r, CR, "\n")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "D ", r.RecordType)
 	assert.Equal(t, 3, r.UnitNo)
@@ -54,18 +53,90 @@ type TestBinaryStructure1 struct {
 	assert.Equal(t, "", r.TestResults[1].Flags)
 
 }
-*/
+
 type TestBinaryStructure2 struct {
 	DMessage []TestBinaryStructure1
 }
 
-func TestUnmarshalBinary2(t *testing.T) {
+// This test inspired by the AU-600 protocol:
+// Read two consecutive Data messages
+func TestUnmarshalMultipleRecords(t *testing.T) {
 	data := "D 03116506 044760722905768    E61     6.40  62      935  \u000DD 03116507 044860722905758    E61     6.86  62      883  "
 
 	var r TestBinaryStructure2
-	err := Unmarshal([]byte(data), &r, CR, "\r")
+	err := Unmarshal([]byte(data), &r, "\r")
 
 	assert.Equal(t, nil, err)
 
 	assert.Equal(t, 2, len(r.DMessage))
+	assert.Equal(t, "D ", r.DMessage[0].RecordType)
+	assert.Equal(t, 3, r.DMessage[0].UnitNo)
+	assert.Equal(t, 1165, r.DMessage[0].RackNumber)
+	assert.Equal(t, 6, r.DMessage[0].CupPosition)
+	assert.Equal(t, "", r.DMessage[0].SampleType)
+	assert.Equal(t, "0447", r.DMessage[0].SampleNo)
+	assert.Equal(t, "60722905768", r.DMessage[0].SampleId)
+	assert.Equal(t, "", r.DMessage[0].Dummy)
+	assert.Equal(t, "E", r.DMessage[0].BlockIdentification)
+
+	assert.Equal(t, 2, len(r.DMessage[0].TestResults))
+
+	assert.Equal(t, "61", r.DMessage[0].TestResults[0].TestCode)
+	assert.Equal(t, "6.40", r.DMessage[0].TestResults[0].TestResult)
+	assert.Equal(t, "", r.DMessage[0].TestResults[0].Flags)
+
+	assert.Equal(t, "62", r.DMessage[0].TestResults[1].TestCode)
+	assert.Equal(t, "935", r.DMessage[0].TestResults[1].TestResult)
+	assert.Equal(t, "", r.DMessage[0].TestResults[1].Flags)
+
+	//--------------------------- 2nd message block
+	assert.Equal(t, "D ", r.DMessage[1].RecordType)
+	assert.Equal(t, 3, r.DMessage[1].UnitNo)
+	assert.Equal(t, 1165, r.DMessage[1].RackNumber)
+	assert.Equal(t, 7, r.DMessage[1].CupPosition)
+	assert.Equal(t, "", r.DMessage[1].SampleType)
+	assert.Equal(t, "0448", r.DMessage[1].SampleNo)
+	assert.Equal(t, "60722905758", r.DMessage[1].SampleId)
+	assert.Equal(t, "", r.DMessage[1].Dummy)
+	assert.Equal(t, "E", r.DMessage[1].BlockIdentification)
+
+	assert.Equal(t, 2, len(r.DMessage[1].TestResults))
+
+	assert.Equal(t, "61", r.DMessage[1].TestResults[0].TestCode)
+	assert.Equal(t, "6.86", r.DMessage[1].TestResults[0].TestResult)
+	assert.Equal(t, "", r.DMessage[1].TestResults[0].Flags)
+
+	assert.Equal(t, "62", r.DMessage[1].TestResults[1].TestCode)
+	assert.Equal(t, "883", r.DMessage[1].TestResults[1].TestResult)
+	assert.Equal(t, "", r.DMessage[1].TestResults[1].Flags)
+}
+
+type invalidAddressAnnoation struct {
+	Field string `bin:"-1:"` // invalid without a length
+}
+
+func TestExpectInvalidAddressAnnotationToFail(t *testing.T) {
+	data := "123456"
+
+	var r invalidAddressAnnoation
+	err := Unmarshal([]byte(data), &r, "\r")
+
+	assert.Equal(t, ErrInvalidAddressAnnotation, err)
+}
+
+type absoluteAddressing struct {
+	Field1 string `bin:"0:2"`
+	Field2 string `bin:"5:2"`
+}
+
+func TestAbsoluteAddressing(t *testing.T) {
+	data := "01xxx56xxx"
+
+	var r absoluteAddressing
+	err := Unmarshal([]byte(data), &r, "\r")
+
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, "01", r.Field1)
+	assert.Equal(t, "56", r.Field2)
 }
